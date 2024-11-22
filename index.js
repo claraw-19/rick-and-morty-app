cd22cdffc580b0370c693caf2e0a7ac6947d0e56
Maria Osypova <mosipova1998@gmail.com>
2024-11-22 12:54:19 +0100

    fetch character data from api and render them into card, only for first 20 characters

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 
-const characterCard = createCharacterCard("url", "Klaus", "Alive", "Coach", 22);
-cardContainer.appendChild(characterCard);
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+  const apiUrl = "https://rickandmortyapi.com/api/character";
+  const response = await fetch(apiUrl);
+  const data = await response.json();
 
-// 1. const character card = fun
-// 2. append card to card conteiner
-// 3. test
+  console.log(data.results);
+
+  const characters = data.results;
+  characters.forEach((character) => {
+    const imageUrl = character.image;
+    const name = character.name;
+    const status = character.status;
+    const characterType = character.type;
+    const occurrences = character.episode.length;
+    const characterCard = createCharacterCard(
+      imageUrl,
+      name,
+      status,
+      characterType,
+      occurrences
+    );
+    cardContainer.appendChild(characterCard);
+  });
+}
+
+fetchCharacters();
+
+//1. create const for api url
+//2. create function fetch characters
+//3. console log for test and check output with function create...
+//4. create parameteres for function create
+//5. array with 20 objects data.results[0].name
+
+//const characterCard = createCharacterCard("url", "Klaus", "Alive", "Coach", 22);
 
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
